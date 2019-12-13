a8fc4719566b144ecb57c71da84cc598de7a4d2c
pacificescape <17690123+pacificescape@users.noreply.github.com>
2019-12-13 20:42:37 +0100

    ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps : [{
+      name: 'FRONTEND',
+      script: 'serve',
+  
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: 'serve -l 443 -s build',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }],
+  
+    deploy : {
+      production : {
+        user : 'pacificescape',
+        host : '212.83.163.1',
+        ref  : 'origin/master',
+        repo : 'git@github.com:pacificescape/steakerstats-front.git',
+        path : '.',
+        'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+      }
+    }
+  };
