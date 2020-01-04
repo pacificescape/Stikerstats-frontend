8f0b6b177e98749685655b874a07404098bdb8b6
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-01-04 04:52:28 +0100

    widget fix

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -2,9 +2,22 @@ import React from 'react'
 import styles from './Widget.module.css'
 
 export default () => {
+
+    const script = document.createElement("script");
+
+    script.src = "https://comments.app/js/widget.js?2";
+    script.async = true;
+    script['data-comments-app-website'] = "lqba9khA"
+    script['data-limit']="10"
+    script['data-color']="015"
+
+    let wrapper = document.getElementsByClassName(styles.widget)
+    wrapper.appendChild(script);
+
+
+
     return (
         <div className={styles.widget}>
-            <script async src="https://comments.app/js/widget.js?2" data-comments-app-website="lqba9khA" data-limit="10" data-color="015"></script>
         </div>
     )
 }
