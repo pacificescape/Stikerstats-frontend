86cf022e02d1bb264d7de2762b71d724fc7e96c3
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-01-04 05:10:13 +0100

    widget test

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './Widget.module.css'
 
-export default () => {
+export default class Widget extends Component {
 
-    const script = document.createElement("script");
-
-    script.src = "https://comments.app/js/widget.js?2";
-    script.async = true;
-    script['data-comments-app-website'] = "lqba9khA"
-    script['data-limit']="10"
-    script['data-color']="015"
-
-    let wrapper = document.getElementsByClassName(styles.widget)
-    wrapper.appendChild(script);
+    componentDidMount() {
 
 
+        let script = document.createElement("script");
 
-    return (
-        <div className={styles.widget}>
-        </div>
-    )
+        script.src = "https://comments.app/js/widget.js?2";
+        script.async = true;
+        script.setAttributeNS('string', 'data-comments-app-website', 'lqba9khA')
+        script.setAttributeNS('string', 'data-limit','10')
+        script.setAttributeNS('string', 'data-color','015')
+
+        let wrapper = document.getElementsByClassName(styles.widget)
+        wrapper[0].appendChild(script);
+    }
+
+
+
+    render() {
+        return (
+            <div className={styles.widget}>
+            </div>
+        )
+    }
 }
